0957144c902e8c7537fe425c06672e650e11b1f7
namistarr <namistarr@icloud.com>
2019-06-20 16:41:45 -0700

    fixed my inactive tabs not going back to red

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,7 +9,7 @@ class TabLink {
     this.data = this.element.dataset.tab;
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    this.itemElement = document.querySelector(`.tabs-item[data-tab = '${this.data}']`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     this.tabItem = new TabItem(this.itemElement);
@@ -22,17 +22,20 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-    const links = document.querySelectorAll('.tabs-links');
+    const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
     Array.from(links).forEach((link) => {
       link.classList.remove('tabs-link-selected');
     })
+    // links.forEach(link => {
+    //   link.classList.remove('tabs-link-selected');
+    // })
     // Add a class named "tabs-link-selected" to this link
     // this.element;
     this.element.classList.add('tabs-link-selected');
     // Call the select method on the item associated with this link
-    this.tabItem.select(this.itemElement);
+    this.tabItem.select();
   }
 }
 
@@ -48,7 +51,7 @@ class TabItem {
     // const items;
     const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
-    items.forEach((item) => {
+    Array.from(items).forEach((item) => {
       item.classList.remove('tabs-item-selected');
     })
     // Add a class named "tabs-item-selected" to this element
